fc47b59eec0cbb44c93a91a494d2f65f43b3b241
cheruiyotrop <cheruiyot.rop@student.moringaschool.com>
2024-03-01 10:10:44 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        fibonacci_sequence = [0, 1]
+        for _ in range(2, length):
+            next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+            fibonacci_sequence.append(next_number)
+        print(fibonacci_sequence)
+
+
+print_fibonacci(10)
